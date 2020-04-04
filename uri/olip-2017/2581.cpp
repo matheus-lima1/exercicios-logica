333d535807b61c352ac876cc97018026216784e0
Matheus Lima <matheus.lima@unifei.edu.br>
2020-04-04 20:50:10 -0300

    olip-resolution1

---
+++ uri/olip-2017/2581.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+
+    int i,n;
+    char x[100];
+    scanf("%d",&n);
+
+    for(i=0 ; i<n ; i++){
+
+        scanf("%s",&x);
+        printf("I am Toorg!\n");
+
+    }
+
+    return 0;
+}
---
+++ uri/olip-2017/2583.py
@@ -0,0 +1,16 @@
+c = int(input())
+vetor = []
+frase = []
+
+
+for i in range(c):
+    n = int(input())
+    print("TOTAL\n")
+    for x in range(n):
+      frase.append(input)
+            
+
+            
+
+    
+    
