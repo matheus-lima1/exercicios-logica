19eebcbab6e39e64ef59a88d379944ca3acde74d
Matheus Lima <matheus.lima@unifei.edu.br>
2020-04-09 16:31:14 -0300

    tentativa-questao8

--- com110/lista2/ex-08.cpp
+++ com110/lista2/ex-08.cpp
@@ -3,20 +3,37 @@
 
 int main(){
 
-    double n1,n2,n2,maior,meio,menor;
+    double n1,n2,n3,maior,menor,meio;
 
     printf("Informe tres numeros: \n");
-    scanf("%lf%lf",&n1,&n2);
+    scanf("%lf%lf%lf",&n1,&n2,&n3);
 
-    if(n1>n2){
+    if(n1>=n2 && n1>=n3 && n2>=n3){
         maior = n1;
+        meio = n2;
+        menor = n3;
+    } else if(n1>=n2 && n1>=n3 && n3>=n2){
+        maior = n1;
+        meio = n3;
         menor = n2;
-    } else {
+    }else if(n2>=n1 && n2>=n3 && n3>=n1){
         maior = n2;
         menor = n1;
+        meio = n3;
+    }else if(n2>=n1 && n2>=n3 && n1>=n3){
+        maior = n2;
+        menor = n3;
+        meio = n1;
+
+    }else if (n3>=n1 && n3>=n2 && n1>=n2){
+        maior = n3;
+        menor = n2;
+        meio = n1;
+    }else if (n3>=n1 && n3>=n2 && n2>=n1){
+        maior = n3;
+        menor = n1;
+        meio = n2;
     }
-
-    printf("Ordem crescente = %.2lf , %.2lf",menor,maior);
-
+    printf("Ordem crescente = %.2lf -> %.2lf -> %.2lf",menor,meio,maior);
     return 0;
 }
