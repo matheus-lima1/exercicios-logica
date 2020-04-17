f9e246f877b8a04bcd1f47624277c930ac384288
Matheus Lima <matheus.lima@unifei.edu.br>
2020-04-16 22:37:08 -0300

    lista3-resolution

--- com110/lista3/ex-09.cpp
+++ com110/lista3/ex-09.cpp
@@ -7,24 +7,6 @@ int main(){
 
     int i=0,x;
 
-
-        while(i!=50){
-            printf("#");
-
-            if(i==24){
-                printf("\n#                       #\n");
-                printf("#                       #\n");
-                printf("#     SEJA BEM-VINDO    #\n");
-                printf("#                       #\n");
-                printf("#                       #\n");
-            }
-
-
-            i++;
-        }
-
-
-        /*
         printf("#######################\n");
         for (x=0 ; x<2; x++){
         printf("#                     #\n");
@@ -34,7 +16,6 @@ int main(){
         printf("#                     #\n");
         }
         printf("#######################\n");
-        */
 
     return 0;
 }
