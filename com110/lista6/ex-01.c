f36037cb764c9056d0231cf18a7206dfe2ce10b8
Matheus Lima <matheus.lima@unifei.edu.br>
2020-05-19 13:19:00 -0300

    finalizado

--- com110/lista6/ex-01.c
+++ com110/lista6/ex-01.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+main(){
+
+    int i, j,vetor[8],aux;
+
+    for(i=0 ; i<8 ; i++){
+        vetor[i] = NULL;
+    }
+
+    for(i=0 ; i<8 ; i++){
+       printf("Vetor[%d]: ",i);
+       scanf("%d",&vetor[i]);
+    }
+
+    for(i=0 ; i<8 ; i++){
+        for(j=0 ; j<8 ; j++){
+            if(vetor[i]<vetor[j]){
+                aux = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = aux;
+            }
+        }
+    }
+
+    printf("\n-----ORDENANDO-----\n");
+
+    for(i=0 ; i<8 ; i++){
+       printf("\nVetor[%d]: %d",i,vetor[i]);
+
+    }
+
+}
