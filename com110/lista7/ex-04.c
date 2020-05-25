c5c0b78170eaa33be636b53143108834a0a54793
Matheus Lima <matheus.lima@unifei.edu.br>
2020-05-25 12:02:59 -0300

    tentativa-1

--- com110/lista7/ex-04.c
+++ com110/lista7/ex-04.c
@@ -4,7 +4,8 @@
 main(){
 
     int linha=2,coluna=2;
-    int i,j,matriz[2][2],ponto,maior,menor;
+    int i,j,x,matriz[2][2],ponto=0;
+    int menorLinha[linha], maiorColuna[coluna],maior,menor;
 
     for(i=0 ; i<2 ; i++){
         for(j=0 ; j<2 ; j++){
@@ -16,11 +17,35 @@ main(){
     maior = matriz[0][0];
     menor = matriz[0][0];
 
+    for(j=0 ; j<coluna ; j++){
+        for(i=0 ; i<linha ; i++){
+            if(matriz[i][j]>maior){
+                maior = matriz[i][j];
+            }
+        }
+        maiorColuna[j] = maior;
+    }
+
     for(i=0 ; i<linha ; i++){
         for(j=0 ; j<coluna ; j++){
-
-
+            if(matriz[i][j]<menor){
+                menor = matriz[i][j];
+            }
         }
+        menorLinha[i] = menor;
+    }
+
+    for(i=0 ; i<linha ; i++){
+        for(j=0 ; j<coluna ; j++){
+            if(menorLinha[i] == maiorColuna[j]){
+                printf("\nPonto de Sela: Matriz[%d][%d] = %d",i,j,matriz[i][j]);
+                ponto++;
+            }
+        }
+    }
+
+    if(ponto == 0){
+        printf("Ponto de sela inexistente");
     }
 
 }
