a17a247dee50c811d3c00484e1175ebc7a3fdda2
Matheus Lima <matheusbmlima2020@gmai.com>
2020-06-02 15:24:33 -0300

    menu-update-1

--- com110/atividade-avaliativa-1/jogo.c
+++ com110/atividade-avaliativa-1/jogo.c
@@ -4,6 +4,7 @@
 #include <conio.h>
 
 
+
 /*
 
 IF-ELSE (ok)
@@ -15,11 +16,38 @@ ARQUVIO E REGISTRI (NÃO)
 */
 
 void exibeMenu(){
+    int i;
 
-    printf("\n\n########## MENU ##########\n");
-    printf("       1 - Jogar          \n");
-    printf("       2 - Sair           \n");
-    printf("Escolha uma alternativa: ");
+    for(i=0 ; i<36 ; i++){
+        printf("#");
+    }
+
+    printf("\n");
+    printf("######### SEJA BEM VINDO ########### \n");
+
+    for(i=0;i<36;i++){
+			printf("#");
+    }
+
+    printf("\n##");
+
+    for(i=0;i<32;i++){
+			printf(" ");
+    }
+
+    printf("## \n");
+    printf("##                                ##\n");
+    printf("## 1. Jogar                       ##\n");
+    printf("## 2. Ver rankig                  ##\n");
+    printf("## 3. Sair                        ##\n");
+    printf("##                                ##\n");
+    printf("##                                ##\n");
+
+    for(i=0;i<36;i++){
+			printf("#");
+    };
+
+    printf("\nOpcao: ");
 
 }
 
@@ -30,9 +58,12 @@ void printaMatriz(int matriz[4][4]){
     for(i=0 ; i<4 ; i++){
             printf("\n");
         for(j=0 ; j<4 ; j++){
-            printf("%d - ",matriz[i][j]);
+            printf("%d\t",matriz[i][j]);
         }
+        printf("\n");
     }
+
+    printf("\n");
 
 }
 
@@ -114,6 +145,37 @@ int geraValores2(int matriz[4][4]){
 }
 
 
+main(){
+
+    int i,opcao,valores[8],matriz[4][4],matrizJogo[4][4];
+
+
+    do{
+        exibeMenu();
+        scanf("%d",&opcao);
+
+        system("cls");
+
+        switch(opcao){
+        case 1:
+            geraValores(matriz);
+            geraValores2(matriz);
+            printaMatriz(matriz);
+
+            // esperar tempo
+
+            zeraMatriz(matrizJogo);
+            printaMatriz(matrizJogo);
+
+        }
+
+
+
+    } while (opcao!=3);
+
+}
+
+
 int zeraMatriz(int matriz[4][4]){
 
     int i,j;
@@ -128,28 +190,6 @@ int zeraMatriz(int matriz[4][4]){
 }
 
 
-main(){
-
-    int i,opcao,valores[8],matriz[4][4];
-
-
-    do{
-        exibeMenu();
-        scanf("%d",&opcao);
-
-        system("cls");
-
-        zeraMatriz(matriz);
-        geraValores(matriz);
-        geraValores2(matriz);
-        getch();
-        printaMatriz(matriz);
-
-
-
-    } while (opcao!=2);
-
-}
 
 
 
